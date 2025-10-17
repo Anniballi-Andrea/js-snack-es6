5ae567c87a6d7509f277c0ea1616482a3f6d043c
Anniballi-Andrea <andreanniballi@gmail.com>
2025-10-17 19:21:07 +0200

    i think it finally work

--- snack2/assets/js/main.js
+++ snack2/assets/js/main.js
@@ -5,7 +5,7 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
 
-const teams =[
+const teams = [
     {
         name: 'Roma',
         point: 0,
@@ -23,7 +23,8 @@ const teams =[
     },
 ];
 //dichiaro il nuovo array
-const newArray=[]
+const newArray = []
+
 
 
 
@@ -33,20 +34,33 @@ for (let i = 0; i < teams.length; i++) {
     //genero i numeri random
     team.point = Math.floor(Math.random() * 100);
     team.fouls = Math.floor(Math.random() * 100);
-    
+
 }
 console.log(teams)
 //devo creare un nuovo object da inserire nel nuovo array
 for (let i = 0; i < teams.length; i++) {
     const team = teams[i];
-    const newObject={}
+    let newObject = {}
+    
     //prendo i le singole key per confrontarle per capire se devono essere inserite nel nuovo array
-    for (const key in teams) {
-    console.log(teams[key])
+    for (let key in team) {
+        //console.log(key)
+        //controllo se sono diverse da point
+        if (key !== 'point') {
+            //in questo caso le inserisco nel nuovo object
+            newObject[key] = team[key]
+
+            //console.log(newObject)
+            
+            
+        }
+        
 
 
-
+    }
+    //pusho il nuovo object dentro il nuovo array
+    newArray.push(newObject)
+    
 }
-
-
-}
+//loggo l'array per vedere se funziona
+console.log(newArray)
