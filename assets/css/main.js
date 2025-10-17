98c4e458be676eecd2894bcbfef1b1e1154ce1a9
Anniballi-Andrea <andreanniballi@gmail.com>
2025-10-17 15:22:04 +0200

    for snak 1 make the array

--- assets/css/main.js
+++ assets/css/main.js
@@ -7,5 +7,36 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 */
 
+//Creare un array di oggetti:
+
+
+const bikeList =[
+    //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+    {
+        name: 'bikeOne',
+        kgWeight: 7,
+
+    },
+
+    {
+        name: 'bikeTwo',
+        kgWeight: 8,
+
+    },
+    {
+        name: 'bikeThree',
+        kgWeight: 6,
+    },
+
+
+
+]
+//Stampare a schermo la bici con peso minore.
+console.log(bikeList)
+console.log(bikeList[0])
+console.log(bikeList[1])
+console.log(bikeList[2])
+
+
 
 
